45920a5d830f78606fb8a5bbbbf087f38d066734
yury blinov <yuriy.blinov@profitclicks.info>
2022-02-18 12:14:53 +0300

    хедер списка компаний

--- src/pages/compaings.vue
+++ src/pages/compaings.vue
@@ -1,7 +1,69 @@
 <template>
   <q-page class="compaings pt-0">
-    <!-- icon="mail" -->
+    <!-- icon="mail" q-btn-dropdown__arrow -->
     <div class="head">
+      <q-btn-dropdown
+        outline
+        icon="svguse:icons/allIcons.svg#calendar"
+        class="dropdown-datepicker"
+        :label="`${date.from} - ${date.to}`"
+        no-caps
+      >
+        <q-card class="row datepicker">
+          <q-list dense bordered padding class="rounded-borders">
+            <q-item>
+              <q-item-section>Сегодня</q-item-section>
+            </q-item>
+
+            <q-item>
+              <q-item-section>Вчера</q-item-section>
+            </q-item>
+
+            <q-item>
+              <q-item-section>Позавчера</q-item-section>
+            </q-item>
+
+            <q-item class="q-item--active">
+              <q-item-section>Текущая неделя</q-item-section>
+            </q-item>
+
+            <q-item>
+              <q-item-section>Прошлая неделя</q-item-section>
+            </q-item>
+
+            <q-item>
+              <q-item-section>Текущий месяц</q-item-section>
+            </q-item>
+
+            <q-item>
+              <q-item-section>Прошлый месяц</q-item-section>
+            </q-item>
+
+            <q-item>
+              <q-item-section>- 7 дней</q-item-section>
+            </q-item>
+
+            <q-item>
+              <q-item-section>- 30 дней</q-item-section>
+            </q-item>
+
+            <q-item>
+              <q-item-section>- 90 дней</q-item-section>
+            </q-item>
+          </q-list>
+
+          <q-date
+            v-model="date"
+            minimal
+            range
+            mask="DD.MM.YYYY"
+          >
+          </q-date>
+        </q-card>
+      </q-btn-dropdown>
+      <!-- <q-input filled v-model="date.from">
+        
+      </q-input> -->
       <div class="head-filter row">
         <q-input
           outlined
@@ -27,7 +89,8 @@ export default ({
   name: 'compaings',
   setup () {
     return {
-      model: ref('')
+      model: ref(''),
+      date: ref({ "from": "01.02.2022", "to": "16.02.2022" })
     }
   }
 })
