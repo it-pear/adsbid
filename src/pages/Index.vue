52c1bcd5726f0baf590ca8c1d89e368e8ecd6257
itpear <yurecblinovgelarm@gmail.com>
2022-02-21 17:22:15 +0300

    десктоп главной полоностью готов

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -11,13 +11,13 @@
           v-model="single"
           :options="options"
           dropdown-icon="keyboard_arrow_down"
-          label="Выберит значение"
+          label="Выберите значение"
         />
         <q-select
           v-model="single2"
           :options="options2"
           dropdown-icon="keyboard_arrow_down"
-          label="Выберит значение"
+          label="Выберите значение"
         />
       </div>
       <div class="feed-cards">
@@ -47,14 +47,102 @@
         />
       </div>
     </section>
-    <section class="sidebar">
+    <div class="sidebar">
+      <section class="geo">
+        <div class="row justify-between items-center head">
+          <div class="sidebar__title">Популярные ГЕО</div>
+          <q-tabs v-model="tab" class="tabs" no-caps>
+            <q-tab class="tab" name="today" label="За сегодня" />
+            <q-tab class="tab" name="week" label="За неделю" />
+          </q-tabs>
+        </div>
+        <q-table
+          hide-pagination
+          row-key="id"
+          :hide-selected-banner="true"
+          :binary-state-sort="true"
+          :rows="rows"
+          :columns="columns"
+          class="q-table-home"
+          v-if="!nulldate"
+        >  
+          <template v-slot:body-cell-country="props">
+            <q-td :props="props">
+              <div class="q-table__country">
+                <q-icon size="29px" class="q-icon__del" :name="`svguse:icons/flags.svg#${props.row.country}`" />
+                <div class="q-table__country-reduction">{{props.row.country}}</div>
+                <div class="q-table__country-title">{{props.row.labelcountry}}</div>
+              </div>
+            </q-td>
+          </template> 
+          <template v-slot:body-cell-clicks="props">
+            <q-td :props="props">
+              <div class="q-table__clicks">
+                {{props.row.clicks}}
+              </div>
+            </q-td>
+          </template> 
+        </q-table>
 
-    </section>
+      </section>
+      <section class="transit">
+        <div class="row justify-between items-center head">
+          <div class="sidebar__title">Переходы</div>
+          <q-select
+            v-model="single3"
+            :options="options3"
+            dropdown-icon="keyboard_arrow_down"
+            label="Выберите значение"
+          />
+          <img src="~assets/feed-grafic.svg" />
+        </div>
+      </section>
+    </div>
+
   </q-page>
 </template>
 
 <script>
 import { defineComponent, ref } from 'vue';
+const columns = [
+  {
+    name: 'country',
+    required: true,
+    label: 'Страна',
+    align: 'left',
+    field: row => row.name,
+    format: val => `${val}`,
+    sortable: false
+  },
+  { name: 'clicks', align: 'left', label: 'Клики', field: 'clicks', sortable: false }
+]
+const rows = [
+  {
+    country: 'ru',
+    labelcountry: 'Россия',
+    clicks: '500 000'
+  },
+  {
+    country: 'ua',
+    labelcountry: 'Украина',
+    clicks: '250 000'
+  },
+  {
+    country: 'kz',
+    labelcountry: 'Казахстан',
+    clicks: '500 000'
+  },
+  {
+    country: 'ru',
+    labelcountry: 'Россия',
+    clicks: '99 000'
+  },
+  {
+    country: 'ru',
+    labelcountry: 'Россия',
+    clicks: '32 000'
+  },
+]
 
 export default defineComponent({
   setup() {
@@ -76,6 +164,13 @@ export default defineComponent({
         "Apple",
         "Oracle"
       ],
+      single3: ref('За сегодня'),
+      options3: [
+        "За сегодня",
+        "За вчера",
+        "7 дней",
+        "За месяц"
+      ],
       cards: [
         {img: '/images/Frame_1.jpg'},
         {img: '/images/Frame_2.jpg'},
@@ -87,7 +182,10 @@ export default defineComponent({
         {img: '/images/Frame_2.jpg'},
         {img: '/images/Frame_3.jpg'}
       ],
-      current: ref(1)
+      tab: ref('today'),
+      current: ref(1),
+      columns,
+      rows
     }
   }
 })
